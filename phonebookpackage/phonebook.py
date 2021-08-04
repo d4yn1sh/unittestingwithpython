7e109ad32961a9cb30a44233d4c52d940523037a
Dennis Groisman <dennis@texel.live>
2021-08-04 21:20:55 +0300

    Added more test cases

--- phonebookpackage/phonebook.py
+++ phonebookpackage/phonebook.py
@@ -9,4 +9,16 @@ class PhoneBook:
 		return self.numbers[name]
 
 	def is_consistent(self):
-		pass
+		for name1, number1 in self.numbers.items():
+			for name2, number2 in self.numbers.items():
+				if name1 == name2:
+					continue
+				if number1.startswith(number2):
+					return False
+		return True
+
+	def get_all_numbers(self):
+		return self.numbers.values()
+
+	def get_all_names(self):
+		return self.numbers.keys()
--- phonebookpackage/test_phonebook.py
+++ phonebookpackage/test_phonebook.py
@@ -18,9 +18,28 @@ class PhoneBookTest(unittest.TestCase):
 		with self.assertRaises(KeyError):
 			self.phoneBook.lookup("Alice")
 
-	@unittest.skip("WIP")
 	def test_empty_phonebook_is_consistent(self):
-		self.assertTrue(self.phoneBook.is_consistent)
+		self.assertTrue(self.phoneBook.is_consistent())
+
+	def test_is_consistent(self):
+		self.phoneBook.add("Bob", "12345")
+		self.phoneBook.add("Anna", "012345")
+		self.assertTrue(self.phoneBook.is_consistent())
+
+	def test_inconsistent_with_duplicate_entries(self):
+		self.phoneBook.add("Bob", "12345")
+		self.phoneBook.add("Sue", "12345")
+		self.assertFalse(self.phoneBook.is_consistent())
+
+	def test_inconsistent_with_duplicate_prefix(self):
+		self.phoneBook.add("Bob", "12345")
+		self.phoneBook.add("Sue", "123")
+		self.assertFalse(self.phoneBook.is_consistent())
+
+	def test_phonebook_adds_names_and_numbers(self):
+		self.phoneBook.add("Bob", "12345")
+		self.assertIn("Bob", self.phoneBook.get_all_names())
+		self.assertIn("12345", self.phoneBook.get_all_numbers())
 
 
 if __name__ == "__main__":
